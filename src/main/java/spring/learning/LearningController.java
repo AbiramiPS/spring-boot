9db7c015d0fb40ac6c624401572994327acec0aa
ABIRAMI P S <74205671+AbiramiPS@users.noreply.github.com>
2026-05-26 17:00:23 +0000

    spring boot setup

---
+++ src/main/java/spring/learning/LearningController.java
@@ -0,0 +1,13 @@
+package spring.learning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LearningController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
